9239d72e84f67d06ad37c298d51115377c4ef2db
Diego Arenas <diegoarenas97258@correo.itm.edu.co>
2024-10-17 17:52:35 -0500

    Fake commit

--- lib/pages/sections/detail_api_book_page.dart
+++ lib/pages/sections/detail_api_book_page.dart
@@ -15,6 +15,7 @@ class _DetailApiBookPageState extends State<DetailApiBookPage> {
 
   _DetailApiBookPageState(this.book);
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
